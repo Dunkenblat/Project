2963fd5ce2b4e4ffa9347a19f3012b3d5da1fe8c
Dunkenblat <56467988+Dunkenblat@users.noreply.github.com>
2020-01-17 15:12:48 +0100

    Update 2.c

--- elementarz_2/2.c
+++ elementarz_2/2.c
@@ -1,17 +1,3 @@
-//2. Zamiast komentarzy /* ... */ w programie poniżej wpisz kod tak, aby powstał program, który sprawdza czy podana liczba całkowita jest pierwsza.
-
-//#include <stdio.h>
-//int main() {
-  /* deklaracje zmiennych */
-//printf("Podaj liczbe calkowita: ");
-  /* wczytaj tę liczbę do zmiennej n */
-  /* sprawdź czy n jest liczbą pierwszą */
-//  printf("Podana liczba '%d' jest %s\n",
-//    n, jest_pierwsza ? "pierwsza" : "złożona");
-// return 0;
-//}
-
-
 #include <stdio.h>
 
 int main() {
