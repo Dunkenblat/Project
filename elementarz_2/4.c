d5fdb5dde4506cde762b15b606c06b26f781a0cb
Dunkenblat <56467988+Dunkenblat@users.noreply.github.com>
2020-01-17 15:10:51 +0100

    Update 4.c

--- elementarz_2/4.c
+++ elementarz_2/4.c
@@ -1,19 +1,3 @@
-//4. Zamiast komentarza /** ... */ i kropek ... poniżej wpisz kod, tak aby powstał
-//program zliczający znaki odstępu, tabulacji i nowego wiersza tekstu wczytanego z stdin.
-//#include <stdio.h>
-//int main() {
-//   /** deklaracje zmiennych lokalnych */
-//   nb = 0;  /* liczba znaków odstępu */
-//   nt = 0;  /* liczba znaków tabulacji */
-//   nl = 0;  /* liczba znaków nowego wiersza */
-//   while ((c=getchar()) != EOF) {
-//      /** zliczaj białe znaki */
-//   }
-//   printf("liczba znaków odstępu = %d,
-//      tabulacji = %d, nowego wiersza = %d\n", ... );
-//   return 0;
-//}
-
 #include <stdio.h>
 
 int main () {
