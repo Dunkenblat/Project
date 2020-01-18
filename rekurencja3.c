d1056d3d29be88c8721fc42a2a7b1304d57b313c
Dunkenblat <56467988+Dunkenblat@users.noreply.github.com>
2020-01-18 09:16:26 +0100

    Update rekurencja3.c

--- rekurencja3.c
+++ rekurencja3.c
@@ -1,1 +1,22 @@
 //3. Napisać funkcję rekurencyjną odwracającą kolejność liter w napisie podanym na wejściu.
+
+#include <stdio.h>
+#include <string.h>
+
+void swap (char* a, char* b) {
+  char c; c=*a; *a=*b; *b=c;
+}
+void rewers(char* str, int l, int r) {
+	if (r <= l) return;
+	swap(str+l, str+r);
+	rewers(str, l+1, r-1);
+}
+int main(void) {
+	char buf[50];
+
+	printf("podaj łańcuch: ");
+	scanf("%s", buf);
+	rewers(buf, 0, strlen(buf)-1);
+	printf("Łańcuch odwrócony: \"%s\"\n", buf);
+	return 0;
+}
