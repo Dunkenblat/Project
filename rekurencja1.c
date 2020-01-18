97469bf549510ed1e89961553c849ff6f2a025a5
Dunkenblat <56467988+Dunkenblat@users.noreply.github.com>
2020-01-18 10:06:19 +0100

    Update rekurencja1.c

--- rekurencja1.c
+++ rekurencja1.c
@@ -2,14 +2,12 @@
 
 #include <stdio.h>
 
-long pow_rekurencja(long n, int k)
-{
+long pow_rekurencja(long n, int k) {
 	if (k < 1) return 0;
 	if (k == 1) return n;
 	return n*pow_rekurencja(n,k-1);
 }
-int main(void)
-{
+int main(void) {
 	char buf[50];
 	int n, k;
 	long wynik;
@@ -22,6 +20,5 @@ int main(void)
 
 	wynik = pow_rekurencja(n,k);
 	printf("wynik to: %ld\n", wynik);
-
 	return 0;
 }
