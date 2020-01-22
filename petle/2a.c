00d0c476f89a1123c2741e9937b872ab558894eb
Dunkenblat <kamil.danelczyk@onet.pl>
2020-01-22 15:55:30 +0100

    add 2a.c

---
+++ petle/2a.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+int main() {
+  int i, j, k, a, b = 0;
+  printf("Podaj szerokość szachownicy:\n");
+  scanf("%d", &a);
+  printf("podaj wysokość szachownicy:\n");
+  scanf("%d", &b);
+  printf("\n");
+
+  for(i = 1; i <= b; i++){
+    for(j = 1; j <= a; j++){
+      printf("* ");
+    }
+    printf("\n");
+    for(k = 1; k <= a; k++){
+      printf(" *");
+    }
+    printf("\n");
+  }
+  return 0;
+}
