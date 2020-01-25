c7c07cb5bbb9d29f6372366c6275cf044a6326c0
Dunkenblat <56467988+Dunkenblat@users.noreply.github.com>
2020-01-25 10:33:31 +0100

    Create egzamin.c

---
+++ egzamin.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+int i;
+for(i = 0; i++<10; i++)
+printf("%d", i);
+return 0;
+}
+
+/*int main(){
+int i;
+for(i = 0; i++<10; i++)
+printf("%d", i);
+return 0;
+}
+*/
+
+/*int main(){
+int a = 5, b= 0;
+if (a++ == 5 || b++)
+printf("%d %d", a, b);
+else
+printf("%d %d", b, a);
+return 0;
+}
+*/
+
+/*int main(){
+int tab[] = {1, 4, 8};
+int *a = tab;
+int w = *a + 2;
+printf("%d\n", w);
+}
+
+*/
+/*1. b)
+  2. c)
+  3. do wczytania zmiennej jest adres & więc a)
+  4.
+  5. a)
+  6. c)
+  7. a)
+  8. b)
+  9. c)
+
+*/
